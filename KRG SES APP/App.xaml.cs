231b28bed60dd3226c348aa2abe2a91ca9b78388
Sean <Sean@SEANS-LAPTOP>
2021-07-28 18:55:37 +1000

    Add project files.

---
+++ KRG SES APP/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using KRG_SES_APP.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KRG_SES_APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AvailabilityPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
